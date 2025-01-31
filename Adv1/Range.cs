64e7c8c4ec77466030ad54328aaceb44bed603f0
Hassaan-Moahamed <hsanm857@gmail.com>
2025-01-31 21:06:26 +0200

    Add project files.

---
+++ Adv1/Range.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adv1
+{
+    public class Range<T> where T : IComparable<T>
+    {
+        public T MinValue { get; set; }
+        public T MaxValue { get; set; }
+
+        public Range(T minValue, T maxValue)
+        {
+            MinValue = minValue;
+            MaxValue = maxValue;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(MinValue) >= 0 && value.CompareTo(MaxValue) <= 0;
+        }
+
+        public T Length()
+        {
+            dynamic max = MaxValue;
+            dynamic min = MinValue;
+
+            return max - min;
+        }
+    }
+}
